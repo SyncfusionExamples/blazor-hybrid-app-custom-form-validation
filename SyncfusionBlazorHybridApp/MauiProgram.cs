99731e874d52f6fa390350e12353ec90cbf786c7
Saravanan G <saravanang@syncfusion.com>
2023-07-03 16:23:53 +0530

    Added files
    File has been added

---
+++ SyncfusionBlazorHybridApp/MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using SyncfusionBlazorHybridApp.Data;
+using Syncfusion.Blazor;
+
+namespace SyncfusionBlazorHybridApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            //Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
+
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddSyncfusionBlazor();
+            return builder.Build();
+        }
+    }
+}
